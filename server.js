01dd661a5737a29c9648f847695ae2970ee84f6d
weiguifei <weiguifei@fubangyun.com>
2018-10-08 11:40:04 +0800

    123

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//引入http模块
-var http = require("http");
-//设置主机名
-var hostName = '127.0.0.1';
-//设置端口
-var port = 80;
-//创建服务
-var server = http.createServer(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("hello nodejs");
+const express = require('express'); //express框架模块
+const path = require('path'); //系统路径模块
+const app = express();
 
-});
-server.listen(port, hostName, function () {
+const hostName = '127.0.0.1'; //ip
+const port = 8080; //端口
+
+app.use(express.static(path.join(__dirname, 'html'))); //指定静态文件目录
+
+app.listen(port, hostName, function() {
     console.log(`服务器运行在http://${hostName}:${port}`);
 });
