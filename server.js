8466c04b6c3202b67af84a0cd523036882e6520b
weiguifei <weiguifei@fubangyun.com>
2018-10-09 10:20:45 +0800

    12

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); //express框架模块
 const path = require('path'); //系统路径模块
 const app = express();
 
-const hostName = 'localhost'; //ip
+const hostName = '127.0.0.1'; //ip
 const port = 8080; //端口
 
 app.use(express.static(path.join(__dirname, 'html'))); //指定静态文件目录
